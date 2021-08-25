db34dcdd964ce9d908b1d95a120911367e9433ed
Robert Guo <robert.guo@mongodb.com>
2021-08-25 09:41:53 -0400

    add unit tests

--- tests/test_rules.py
+++ tests/test_rules.py
@@ -1022,15 +1022,7 @@ class TestDependencyForFunc(unittest.TestCase):
 class TestTasksForVariant(unittest.TestCase):
     def setUp(self) -> None:
         self.rule = rules.tasks_for_variants.TasksForVariants()
-
-    def test_dupe_variant_in_config(self):
-        pass
-
-    def test_unknown_variant_in_config(self):
-        pass
-
-    def test_mismatched_tasks(self):
-        config = {
+        self.good_config = {
             "task-variant-mappings": [
                 {"name": "mapping1", "tasks": ["t1", "t2"], "variants": ["v1", "v2"]},
                 {"name": "mapping2", "tasks": ["t1", "t2", "t3"], "variants": ["v3"]},
@@ -1038,7 +1030,7 @@ class TestTasksForVariant(unittest.TestCase):
             ]
         }
 
-        good_yaml = load(
+        self.good_yaml = load(
             """
     buildvariants:
     - name: v1
@@ -1057,8 +1049,62 @@ class TestTasksForVariant(unittest.TestCase):
     - name: v4
       tasks:
       - name: t4
+    - name: v_dupe
+      tasks:
+      - name: t3
             """
         )
 
-        violations = self.rule(config, good_yaml)
+    def test_dupe_variant_in_config(self):
+        dupe_variant_config = {
+            "task-variant-mappings": [
+                {"name": "mapping1", "tasks": ["t1", "t2"], "variants": ["v_dupe", "v2"]},
+                {"name": "mapping2", "tasks": ["t3"], "variants": ["v_dupe"]},
+            ]
+        }
+
+        violations = self.rule(dupe_variant_config, self.good_yaml)
+        self.assertEqual(len(violations), 1)
+        self.assertIn("more than once", violations[0])
+
+    def test_unknown_variant_in_config(self):
+        typo_variant_config = {
+            "task-variant-mappings": [
+                {"name": "mapping1", "tasks": ["t1", "t2"], "variants": ["v1", "v2"]},
+                {"name": "mapping2", "tasks": ["t1", "t2", "t3"], "variants": ["v_typo"]},
+            ]
+        }
+
+        violations = self.rule(typo_variant_config, self.good_yaml)
+        self.assertEqual(len(violations), 1)
+        self.assertIn("unknown variant", violations[0])
+
+    def test_conforming_tasks(self):
+        violations = self.rule(self.good_config, self.good_yaml)
         self.assertEqual(violations, [])
+
+    def test_mismatched_tasks(self):
+        bad_yaml = load(
+            """
+    buildvariants:
+    - name: v1
+      tasks:
+      - name: t1
+      - name: t2
+    - name: v2
+      tasks:
+      - name: t1
+      - name: t2
+      - name: t3  # Extra task.
+    - name: v3
+      tasks:
+      - name: t2  # Missing task.
+      - name: t3
+    - name: v4
+      tasks:
+      - name: t4  # Correct task list.
+            """
+        )
+
+        violations = self.rule(self.good_config, bad_yaml)
+        self.assertEqual(len(violations), 2)
