4efceeb8ef4e52979b8dae7a226e09c708600ca8
Mikhail Shchatko <mikhail.shchatko@gmail.com>
2024-04-08 19:45:39 +0300

    DEVPROD-4967 Add variant tags linting (#4)

--- evergreen_lint/rules/__init__.py
+++ evergreen_lint/rules/__init__.py
@@ -12,6 +12,7 @@ from evergreen_lint.rules.commonsense import (
 )
 from evergreen_lint.rules.dependency_for_func import DependencyForFunc
 from evergreen_lint.rules.enforce_tags_for_tasks import EnforceTagsForTasks
+from evergreen_lint.rules.enforce_tags_for_variants import EnforceTagsForVariants
 from evergreen_lint.rules.invalid_build_parameter import InvalidBuildParameter
 from evergreen_lint.rules.required_expansions_write import RequiredExpansionsWrite
 from evergreen_lint.rules.tasks_for_variants import TasksForVariants
@@ -28,6 +29,7 @@ RULES: Dict[str, Type[Rule]] = {
     "dependency-for-func": DependencyForFunc,
     "tasks-for-variants": TasksForVariants,
     "enforce-tags-for-tasks": EnforceTagsForTasks,
+    "enforce-tags-for-variants": EnforceTagsForVariants,
 }
 # Thoughts on Writing Rules
 # - see .helpers for reliable iteration helpers
