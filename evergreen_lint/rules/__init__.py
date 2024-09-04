63b018ef24b873f8f32ef40286ad524698a28b8f
Juan Gu <juan.gu@mongodb.com>
2024-09-04 20:12:21 +0000

    lint

--- evergreen_lint/rules/__init__.py
+++ evergreen_lint/rules/__init__.py
@@ -13,10 +13,10 @@ from evergreen_lint.rules.commonsense import (
 from evergreen_lint.rules.dependency_for_func import DependencyForFunc
 from evergreen_lint.rules.enforce_tags_for_tasks import EnforceTagsForTasks
 from evergreen_lint.rules.enforce_tags_for_variants import EnforceTagsForVariants
-from evergreen_lint.rules.invalid_build_parameter import InvalidBuildParameter
 from evergreen_lint.rules.forbid_tasks_with_tag_on_variants import (
     ForbidTasksWithTagOnVariants,
 )
+from evergreen_lint.rules.invalid_build_parameter import InvalidBuildParameter
 from evergreen_lint.rules.required_expansions_write import RequiredExpansionsWrite
 from evergreen_lint.rules.tasks_for_variants import TasksForVariants
 from evergreen_lint.rules.variant_expansions import VariantExpansions
